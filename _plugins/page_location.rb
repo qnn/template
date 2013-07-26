aefd53cb5fb9f3459b69dc1f2144e808cb58044b
Choi Goon-ho <caiguanhao@gmail.com>
2013-07-26 15:01:42 +0800

    fix for jekyll 1.1.2

--- _plugins/page_location.rb
+++ _plugins/page_location.rb
@@ -12,7 +12,9 @@ module Jekyll
 	class Post
 		alias_method :to_liquid_alias, :to_liquid
 
-		def to_liquid
+		# add one parameter
+		# https://github.com/mojombo/jekyll/blob/v1.1.2/lib/jekyll/post.rb#L278
+		def to_liquid(attrs = ATTRIBUTES_FOR_LIQUID)
 			to_liquid_alias.deep_merge({
 				'location' =>  @base.sub(site.source, '') + '/' + self.name
 			})
